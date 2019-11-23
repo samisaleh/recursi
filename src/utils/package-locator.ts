a73e01dedbb79742a6c9a72e226bc471df9fa676
samisaleh <sami@samisaleh.com>
2019-11-23 17:15:46 -0500

    update to use absolute paths

--- src/utils/package-locator.ts
+++ src/utils/package-locator.ts
@@ -1,8 +1,12 @@
 import fg from 'fast-glob';
 
-export const findPackageDirectories = function(startDir = '.', excludes: string[]): string[] {
-    const filePattern = `${startDir}/**/package.json`;
-    const packageLocations = fg.sync(filePattern, { ignore: ['node_modules', '**/node_modules', ...excludes] });
+export const findPackageDirectories = function(startDir = './', excludes: string[]): string[] {
+    const filePattern = `**/package.json`;
+    const packageLocations = fg.sync(filePattern, {
+        absolute: true,
+        cwd: startDir,
+        ignore: ['**/node_modules', ...excludes],
+    });
 
     if (!packageLocations.length) {
         console.info('No package files could be found in the directory specified');
